bec8b15fc39d6f5efae59fb89dcca362e67bbd3e
Sagar Pawar <sgrpwr@users.noreply.github.com>
2022-03-09 14:39:24 +0530

    Add files via upload

---
+++ Easy/DepthOfTree.java
@@ -0,0 +1,31 @@
+package Java.Basic.Tree;
+
+public class DepthOfTree {
+    public static void main(String[] args) {
+
+    }
+}
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    //Shortest way, Movie theater example would be best to understand- Ask first row which seat are you on and add one to adjacent.
+    public int maxDepth(TreeNode root) {
+        if(root==null) return 0;
+        return Math.max(maxDepth(root.left), maxDepth(root.right))+1;
+    }
+}
